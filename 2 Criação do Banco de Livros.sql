23fa526d8d2f7ed3c4d7ec2eff320353b97e1929
Patrick Domênico dos Santos Pereira Alves <patrickdomeni61@gmail.com>
2026-05-15 17:40:47 -0300

    Adicionou foreign key "generos" para a tabela "livros"

--- 2 Criação do Banco de Livros.sql
+++ 2 Criação do Banco de Livros.sql
@@ -3,12 +3,13 @@ USE livr_ar;
 CREATE TABLE livros (
     id_livro INT AUTO_INCREMENT PRIMARY KEY,
     id_usuario INT NOT NULL,
+    id_genero INT NOT NULL,
     titulo VARCHAR(255) NOT NULL,
     preco DECIMAL(10, 2) NOT NULL DEFAULT 0.00,
-    genero ENUM('Mistério', 'Ficção Cientifica', 'Ação', 'Medieval', '') NOT NULL,
     condicao ENUM('Novo', 'Seminovo', 'Bom estado', 'Com marcas de uso', 'Danificado') NOT NULL,
     cadastrado_em TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    CONSTRAINT fk_usuario_livro FOREIGN KEY (id_usuario) REFERENCES usuarios (id) ON DELETE CASCADE
+    CONSTRAINT fk_usuario_livro FOREIGN KEY (id_usuario) REFERENCES usuarios (id) ON DELETE CASCADE,
+    CONSTRAINT fk_livro_genero FOREIGN KEY (id_genero) REFERENCES generos (id_genero)
 );
 
-SELECT * FROM livros
+SELECT * FROM livros;
