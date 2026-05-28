6a3a0d61e1a52b9d61ecf8f53d77d096b595e557
Patrick Domênico dos Santos Pereira Alves <patrickdomeni61@gmail.com>
2026-05-28 17:52:36 -0300

    Refatorou query SQL para incluir filtros de genero e tipo

--- pesquisa.php
+++ pesquisa.php
@@ -1,44 +1,28 @@
 <?php
-session_start();
 
-// 1. Verifica se o usuário está logado para obter o id_usuario e associar o livro a ele
-if (!isset($_SESSION['usuario_id']) || $_SESSION['usuario_id'] === NULL) {
-    header ("Location: login_view.php"); // 2. Se o usuario não estiver logado, redireciona para a página de login
-}
-
-// 3. Configurações do Banco de Dados
-$host = 'localhost';
-$nome_bd = 'livr_ar';
-$usuario_bd = 'root';
-$senha_bd = '';
-
-$mensagem = '';
-
-try {
-    $pdo = new PDO("mysql:host=$host;dbname=$nome_bd;charset=utf8", $usuario_bd, $senha_bd);
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch(PDOException $e) {
-    error_log("Erro de conexão com o banco de dados: " . $e->getMessage());
-    die("Erro ao conectar ao banco de dados. Tente novamente mais tarde.");
-}
-
-// 4. Inicializa a consulta base com JOIN. Buscamos colunas do livro (l) e o estado do usuário (u)
-$sql = "SELECT l.*, u.estado 
+// 1. Inicializa a consulta base com JOIN. Buscamos colunas do livro (l), o estado do usuário (u) e o gênero (g)
+$sql = "SELECT l.*, u.estado, g.categoria, g.genero AS genero
         FROM livros l 
-        JOIN usuarios u ON l.id_usuario = u.id 
+        LEFT JOIN usuarios u ON l.id_usuario = u.id 
+        LEFT JOIN generos g ON l.id_genero = g.id_genero 
         WHERE 1=1";
 
 $parametros = [];
 
-// 5. Aplicando Filtros se existirem na URL (via GET)
+// 2. Aplicando Filtros se existirem na URL (via GET)
 if (!empty($_GET['titulo'])) {
     $sql .= " AND l.titulo LIKE :titulo";
     $parametros[':titulo'] = '%' . $_GET['titulo'] . '%';
 }
 
-if (!empty($_GET['genero'])) {
-    $sql .= " AND l.genero = :genero";
-    $parametros[':genero'] = $_GET['genero'];
+if (!empty($_GET['tipo'])) {
+    $sql .= " AND g.tipo = :tipo";
+    $parametros[':tipo'] = $_GET['tipo'];
+}
+
+if (!empty($_GET['categoria'])) {
+    $sql .= " AND g.categoria = :categoria";
+    $parametros[':categoria'] = $_GET['categoria'];
 }
 
 if (!empty($_GET['condicao'])) {
